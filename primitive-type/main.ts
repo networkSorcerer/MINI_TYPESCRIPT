1b77de8fb368e7e699e33c2e884ca648f16faf02
networkSocerer <gigattfyhig@gmail.com>
2025-05-26 11:39:13 +0900

    타입스크립트

--- primitive-type/main.ts
+++ primitive-type/main.ts
@@ -11,4 +11,14 @@ let a: number = 3
 a = 4
 a= 9.0
 a= -8.8
-a= "string"
+
+// let b: boolean = true
+// b = false
+
+let b = true
+// 자동으로 타입을 추론 할수 있음 하지만 
+// 그렇게 사용 안하는 게 좋음 
+
+let c :null = null
+
+let d : undefined = undefined
