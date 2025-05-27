a4d0451889ecca0ca64c3798d91c4598b4deb81d
networkSocerer <gigattfyhig@gmail.com>
2025-05-27 14:05:35 +0900

    day2

---
+++ day2/ch3/main.ts
@@ -0,0 +1,7 @@
+interface, type
+
+let b : {name:string|number|boolean, age?:number} = {name:"noona",age:34}
+let c : {name:string,age:number}={name:"noona",age:23}
+
+let d : {name:string|number|boolean, age?:number} ={name:"asd",age:12}
+let e : {name:string|number|boolean, age?:number} = {name:"adsf"}
