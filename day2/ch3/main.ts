d5210d5e0b91c7452957849f65da72b10698d4d5
networdSorcerer <gigattfyhig@gmail.com>
2025-05-27 17:29:56 +0900

    day2

--- day2/ch3/main.ts
+++ day2/ch3/main.ts
@@ -1,6 +1,6 @@
 
 
-interface IStudent {
+interface IStudent { // 객체만 가능한 부분
     name: string;
     age?:number;
     isStudent?:boolean;
@@ -10,12 +10,15 @@ type TStudent = {
     name:string,
     age?:number
 }
-let b : IStudent = {name:"noona",age:34}
 
+type NewType = number | string // 원시타입도 가능, 튜플 타입 
+
+type Tuple = [number,string,boolean]
+
+let b : IStudent = {name:"noona",age:34}
 //let b : {name:string|number|boolean, age?:number} = {name:"noona",age:34}
 
 let c : TStudent={name:"noona",age:23}
-
 // let c : {name:string,age:number}={name:"noona",age:23}
 
 let d : {name:string|number|boolean, age?:number} ={name:"asd",age:12}
