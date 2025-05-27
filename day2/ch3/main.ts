78fd05f0ee8f4d53195ed8f71d6a9b29cf0abc91
networdSorcerer <gigattfyhig@gmail.com>
2025-05-27 17:27:46 +0900

    day2

--- day2/ch3/main.ts
+++ day2/ch3/main.ts
@@ -1,14 +1,22 @@
 
 
-interface Student {
+interface IStudent {
     name: string;
     age?:number;
     isStudent?:boolean;
 }
-let b : Student = {name:"noona",age:34}
+
+type TStudent = {
+    name:string,
+    age?:number
+}
+let b : IStudent = {name:"noona",age:34}
 
 //let b : {name:string|number|boolean, age?:number} = {name:"noona",age:34}
-let c : {name:string,age:number}={name:"noona",age:23}
+
+let c : TStudent={name:"noona",age:23}
+
+// let c : {name:string,age:number}={name:"noona",age:23}
 
 let d : {name:string|number|boolean, age?:number} ={name:"asd",age:12}
 let e : {name:string|number|boolean, age?:number} = {name:"adsf"}
