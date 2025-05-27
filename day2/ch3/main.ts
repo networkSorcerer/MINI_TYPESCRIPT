2bd81ab6bac5a627f7c8fcd3be17063d25c64bcb
networkSorcerer <gigattfyhig@gmail.com>
2025-05-27 17:22:22 +0900

    day2

--- day2/ch3/main.ts
+++ day2/ch3/main.ts
@@ -1,6 +1,13 @@
-interface, type
 
-let b : {name:string|number|boolean, age?:number} = {name:"noona",age:34}
+
+interface Student {
+    name: string;
+    age?:number;
+    isStudent?:boolean;
+}
+let b : Student = {name:"noona",age:34}
+
+//let b : {name:string|number|boolean, age?:number} = {name:"noona",age:34}
 let c : {name:string,age:number}={name:"noona",age:23}
 
 let d : {name:string|number|boolean, age?:number} ={name:"asd",age:12}
