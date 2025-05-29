8160555903f0d0c22d2f48a3e6876d08fa94f719
networkSocerer <gigattfyhig@gmail.com>
2025-05-29 14:16:53 +0900

    day4

--- day4/ch11/main.ts
+++ day4/ch11/main.ts
@@ -3,3 +3,17 @@
 type TString<T> = T extends string ? "yes" : "no";
 
 type reuslt1 = TString<number>;
+
+// 2. mapped type
+
+type TMovie = {
+  title: string;
+  genre: string;
+  rate: number;
+};
+
+type Subset<T> = {
+  [K in keyof T]?: T[K];
+};
+const movie1: Subset<TMovie> = { title: "기생충", genre: "액션" };
+const movie2: Subset<TMovie> = { rate: 2 };
